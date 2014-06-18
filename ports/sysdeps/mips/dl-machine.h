dc388f6f0c919391a39dcf5d10d5323285f057a6
Aurelien Jarno <aurel32@debian.org>
2014-06-18 20:22:44 +0200

    mips/submitted-rld_map.diff
    No DEP3 Subject or Description header found
    
    Gbp-Pq: mips/submitted-rld_map.diff.

--- ports/sysdeps/mips/dl-machine.h
+++ ports/sysdeps/mips/dl-machine.h
@@ -68,7 +68,8 @@
 /* If there is a DT_MIPS_RLD_MAP entry in the dynamic section, fill it in
    with the run-time address of the r_debug structure  */
 #define ELF_MACHINE_DEBUG_SETUP(l,r) \
-do { if ((l)->l_info[DT_MIPS (RLD_MAP)]) \
+do { if ((l)->l_info[DT_MIPS (RLD_MAP)] && \
+	 (l)->l_info[DT_MIPS (RLD_MAP)]->d_un.d_ptr) \
        *(ElfW(Addr) *)((l)->l_info[DT_MIPS (RLD_MAP)]->d_un.d_ptr) = \
        (ElfW(Addr)) (r); \
    } while (0)
