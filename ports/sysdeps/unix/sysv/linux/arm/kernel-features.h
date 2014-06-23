8f02a2449f9081bafb831025430fe536afceb1f6
Aurelien Jarno <aurel32@debian.org>
2014-06-23 20:10:39 +0200

    any/cvs-socketcall-syscall.diff
    No DEP3 Subject or Description header found
    
    Gbp-Pq: any/cvs-socketcall-syscall.diff.

--- ports/sysdeps/unix/sysv/linux/arm/kernel-features.h
+++ ports/sysdeps/unix/sysv/linux/arm/kernel-features.h
@@ -31,9 +31,19 @@
 # define __ASSUME_SIGNALFD4	1
 #endif
 
+/* Support for the recvmmsg syscall was added in 2.6.33.  */
+#if __LINUX_KERNEL_VERSION >= 0x020621
+# define __ASSUME_RECVMMSG_SYSCALL	1
+#endif
+
 /* Support for the accept4 syscall was added in 2.6.36.  */
 #if __LINUX_KERNEL_VERSION >= 0x020624
-# define __ASSUME_ACCEPT4	1
+# define __ASSUME_ACCEPT4_SYSCALL	1
+#endif
+
+/* Support for the sendmmsg syscall was added in 3.0.  */
+#if __LINUX_KERNEL_VERSION >= 0x030000
+# define __ASSUME_SENDMMSG_SYSCALL	1
 #endif
 
 #include_next <kernel-features.h>
