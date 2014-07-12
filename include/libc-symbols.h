f840f894b8afa55ac7d4fb7a97060800cde9344c
Aurelien Jarno <aurel32@debian.org>
2014-07-12 17:22:50 +0200

    Make glibc-2.3.5 compile with gcc-3.4/4.0 + binutils 2.16
    Gbp-Pq: m68k/submitted-gcc34-seccomment.diff.

--- include/libc-symbols.h
+++ include/libc-symbols.h
@@ -202,12 +202,12 @@
 # define __make_section_unallocated(section_string)
 #endif
 
-/* Tacking on "\n\t#" to the section name makes gcc put it's bogus
+/* Tacking on "\n#APP\n\t#" to the section name makes gcc put it's bogus
    section attributes on what looks like a comment to the assembler.  */
 #ifdef HAVE_SECTION_QUOTES
-# define __sec_comment "\"\n\t#\""
+# define __sec_comment "\"\n#APP\n\t#\""
 #else
-# define __sec_comment "\n\t#"
+# define __sec_comment "\n#APP\n\t#"
 #endif
 #define link_warning(symbol, msg) \
   __make_section_unallocated (".gnu.warning." #symbol) \
