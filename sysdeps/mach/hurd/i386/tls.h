1e476947b2d9ec859864b2a56dd7bf6568fb4f63
Aurelien Jarno <aurel32@debian.org>
2014-10-22 20:01:11 +0200

    [PATCH] Hurd: fix port leak in TLS
    Gbp-Pq: hurd-i386/tg-tls_thread_leak.diff.

--- sysdeps/mach/hurd/i386/tls.h
+++ sysdeps/mach/hurd/i386/tls.h
@@ -105,6 +105,8 @@ static inline const char * __attribute__ ((unused))
 _hurd_tls_init (tcbhead_t *tcb, int secondcall)
 {
   HURD_TLS_DESC_DECL (desc, tcb);
+  thread_t self = __mach_thread_self ();
+  const char *msg = NULL;
 
   if (!secondcall)
     {
@@ -112,25 +114,26 @@ _hurd_tls_init (tcbhead_t *tcb, int secondcall)
 	 from the TLS point of view.  */
       tcb->tcb = tcb;
 
-      /* Cache our thread port.  */
-      tcb->self = __mach_thread_self ();
-
       /* Get the first available selector.  */
       int sel = -1;
-      kern_return_t err = __i386_set_gdt (tcb->self, &sel, desc);
+      kern_return_t err = __i386_set_gdt (self, &sel, desc);
       if (err == MIG_BAD_ID)
 	{
 	  /* Old kernel, use a per-thread LDT.  */
 	  sel = 0x27;
-	  err = __i386_set_ldt (tcb->self, sel, &desc, 1);
+	  err = __i386_set_ldt (self, sel, &desc, 1);
 	  assert_perror (err);
 	  if (err)
-	    return "i386_set_ldt failed";
+	    {
+	      msg = "i386_set_ldt failed";
+	      goto out;
+	    }
 	}
       else if (err)
 	{
 	  assert_perror (err); /* Separate from above with different line #. */
-	  return "i386_set_gdt failed";
+	  msg = "i386_set_gdt failed";
+	  goto out;
 	}
 
       /* Now install the new selector.  */
@@ -143,21 +146,29 @@ _hurd_tls_init (tcbhead_t *tcb, int secondcall)
       asm ("mov %%gs, %w0" : "=q" (sel) : "0" (0));
       if (__builtin_expect (sel, 0x48) & 4) /* LDT selector */
 	{
-	  kern_return_t err = __i386_set_ldt (tcb->self, sel, &desc, 1);
+	  kern_return_t err = __i386_set_ldt (self, sel, &desc, 1);
 	  assert_perror (err);
 	  if (err)
-	    return "i386_set_ldt failed";
+	    {
+	      msg = "i386_set_ldt failed";
+	      goto out;
+	    }
 	}
       else
 	{
-	  kern_return_t err = __i386_set_gdt (tcb->self, &sel, desc);
+	  kern_return_t err = __i386_set_gdt (self, &sel, desc);
 	  assert_perror (err);
 	  if (err)
-	    return "i386_set_gdt failed";
+	    {
+	      msg = "i386_set_gdt failed";
+	      goto out;
+	    }
 	}
     }
 
-  return 0;
+out:
+  __mach_port_deallocate (__mach_task_self (), self);
+  return msg;
 }
 
 /* Code to initially initialize the thread pointer.  This might need
