fc7d4a1740b70d79c99fbc158538b29c5d5c9bf6
Balint Reczey <rbalint@ubuntu.com>
2020-11-13 18:54:38 +0100

    2.32-0ubuntu5 (patches unapplied)
    Imported using git-ubuntu import.

--- debian/sysdeps/prof.mk
+++ debian/sysdeps/prof.mk
@@ -1,4 +1,5 @@
 # Library for profiling, built with the same options as libc
+ifeq (,$(filter stage1 stage2,$(DEB_BUILD_PROFILES)))
 ifeq (,$(filter i386 s390x,$(DEB_HOST_ARCH))) # LP: #1898049
   GLIBC_PASSES += prof
   DEB_ARCH_REGULAR_PACKAGES += $(libc)-prof
@@ -7,4 +8,5 @@ ifeq (,$(filter i386 s390x,$(DEB_HOST_ARCH))) # LP: #1898049
   prof_extra_config_options = $(libc_extra_config_options)
   prof_mvec = $(libc_mvec)
   prof_slibdir = /lib/$(libc)-prof/$(DEB_HOST_MULTIARCH)/
+endif
 endif
